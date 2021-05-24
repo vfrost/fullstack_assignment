5a47ef3d16416dcd21035c60f3691c9c41593448
dohtur <alexey.daletsky@gmail.com>
2021-05-24 03:33:20 +0300

    Front done, finally

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    project: './tsconfig.json',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint'
+  ],
+  settings: {
+    react: { version: 'detect' }
+  },
+  rules: {
+    "react/prop-types": 0,
+    "@typescript-eslint/restrict-template-expressions": 0,
+    semi: [ 'error', 'always' ]
+  }
+}
---
+++ frontend/src/clouds/RegionSelector.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Region from './Region';
+
+interface RegionSelectorProps {
+  regions: string[],
+  selectedRegion: string,
+  selectRegion: (region: string) => void
+}
+
+const RegionSelector: React.FC<RegionSelectorProps> = ({regions, selectedRegion, selectRegion}) => {
+  const hasRegions = regions.length > 0;
+
+  if (!hasRegions) return (<></>);
+
+  return(
+    <>
+      <div className=" mt-4 pt-2 border-top text-center">
+        <h4>Available regions</h4>
+      </div>
+      <div className="d-flex justify-content-center flex-wrap">
+        {regions.map(region =>
+          <Region key={region}
+            region={region}
+            selectedRegion={selectedRegion}
+            selectRegion={selectRegion}
+          />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default RegionSelector;
